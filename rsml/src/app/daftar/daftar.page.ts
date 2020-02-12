7d844ecf502b31a7f6ee986b3a0aa74a87086001
Ricky Maulana <maulanaricky45@gmail.com>
2020-02-12 11:26:52 +0700

    Alert Controller, Multiple Choice Poli & Keluhan

--- rsml/src/app/daftar/daftar.page.ts
+++ rsml/src/app/daftar/daftar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-daftar',
@@ -7,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DaftarPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Konfirmasi',
+      message: 'Anda Sudah Yakin?',
+      buttons: [
+        {
+          text: 'Batal',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Yakin',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
   ngOnInit() {
   }
