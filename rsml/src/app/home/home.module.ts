fe738e409963b9082c379ab8ae6382c7e9445944
IvanErwen <kirukato.111@gmail.com>
2020-02-18 12:53:37 +0700

    background

--- rsml/src/app/home/home.module.ts
+++ rsml/src/app/home/home.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
+import 'gl-ionic-background-video';
 import { HomePage } from './home.page';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
   imports: [
@@ -18,6 +19,7 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePageModule {}
