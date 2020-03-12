b4e533342b8ae1fa05f8d9829fbc818ef6be67fb
Ricky Maulana <maulanaricky45@gmail.com>
2020-03-12 14:07:59 +0700

    Login, Register
    Link login to akun

--- rsml/src/app/app-routing.module.ts
+++ rsml/src/app/app-routing.module.ts
@@ -35,6 +35,10 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
 
 ];
 
--- rsml/src/app/login/login.page.ts
+++ rsml/src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
+// import { HomePage } from '../home';
+// import { RegisterPage } from '../register';
+// import { UserAuthenticationService } from '../services/UserAuthentication.service';
 
 @Component({
   selector: 'app-login',
